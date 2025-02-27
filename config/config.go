726c9ad79f2da8db9a03b5d41687467c1da3ba57
firmanmimang <fhidayat131@gmail.com>
2025-02-27 17:46:47 +0700

    feat: create config file

--- config/config.go
+++ config/config.go
@@ -1,1 +1,35 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	PORT        string
+	DB_USER     string
+	DB_PASSWORD string
+	DB_DATABASE string
+	DB_HOST     string
+	DB_PORT     string
+}
+
+var ENV *Config
+
+func LoadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	ENV = &Config{
+		PORT:        os.Getenv("PORT"),
+		DB_USER:     os.Getenv("DB_USER"),
+		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
+		DB_DATABASE: os.Getenv("DB_DATABASE"),
+		DB_HOST:     os.Getenv("DB_HOST"),
+		DB_PORT:     os.Getenv("DB_PORT"),
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/firmanmimang/go-api-trusted-news
 
 go 1.22.3
+
+require github.com/joho/godotenv v1.5.1
