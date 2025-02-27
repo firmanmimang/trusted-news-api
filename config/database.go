00d0af72671cd5f234c8ff241756a7440f594a42
firmanmimang <fhidayat131@gmail.com>
2025-02-27 18:01:42 +0700

    feat: db connect config; call config and connect db on main fun

--- config/database.go
+++ config/database.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Jakarta", ENV.DB_HOST, ENV.DB_USER, ENV.DB_PASSWORD, ENV.DB_DATABASE, ENV.DB_PORT)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect database")
+	}
+
+	DB = db
+
+	log.Println("Database connected")
+}
--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
-func main() {
+import "github.com/firmanmimang/go-api-trusted-news/config"
 
+func main() {
+	config.LoadConfig()
+	config.ConnectDB()
 }
